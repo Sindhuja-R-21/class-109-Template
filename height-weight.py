5be0e45e89c562c47c94e3e83993f684c4ef2100
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2023-06-12 13:25:55 +0530

    Update height-weight.py

--- height-weight.py
+++ height-weight.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import statistics
-import csv
+
 
 df = pd.read_csv("height-weight.csv")
 height_list = df["Height(Inches)"].to_list()
+weight_list = df["Weight(Pounds)"].to_list()
 
 
 #Mean for height and Weight
@@ -38,14 +39,18 @@ height_third_std_deviation_start, height_third_std_deviation_end = height_mean-(
 height_list_of_data_within_1_std_deviation = [result for result in height_list if result > height_first_std_deviation_start and result < height_first_std_deviation_end]
 height_list_of_data_within_2_std_deviation = [result for result in height_list if result > height_second_std_deviation_start and result < height_second_std_deviation_end]
 height_list_of_data_within_3_std_deviation = [result for result in height_list if result > height_third_std_deviation_start and result < height_third_std_deviation_end]
-#Percentage of data within 1, 2 and 3 Standard Deviations for Weight
 
+#Percentage of data within 1, 2 and 3 Standard Deviations for Weight
 weight_list_of_data_within_1_std_deviation = [result for result in weight_list if result > weight_first_std_deviation_start and result < weight_first_std_deviation_end]
 weight_list_of_data_within_2_std_deviation = [result for result in weight_list if result > weight_second_std_deviation_start and result < weight_second_std_deviation_end]
 weight_list_of_data_within_3_std_deviation = [result for result in weight_list if result > weight_third_std_deviation_start and result < weight_third_std_deviation_end]
+
 #Printing data for height and weight (Standard Deviation)
 
 print("{}% of data for height lies within 1 standard deviation".format(len(height_list_of_data_within_1_std_deviation)*100.0/len(height_list)))
 print("{}% of data for height lies within 2 standard deviations".format(len(height_list_of_data_within_2_std_deviation)*100.0/len(height_list)))
 print("{}% of data for height lies within 3 standard deviations".format(len(height_list_of_data_within_3_std_deviation)*100.0/len(height_list)))
 
+print("{}% of data for weight lies within 1 standard deviation".format(len(weight_list_of_data_within_1_std_deviation)*100.0/len(weight_list)))
+print("{}% of data for weight lies within 2 standard deviations".format(len(weight_list_of_data_within_2_std_deviation)*100.0/len(weight_list)))
+print("{}% of data for weight lies within 3 standard deviations".format(len(weight_list_of_data_within_3_std_deviation)*100.0/len(weight_list)))
